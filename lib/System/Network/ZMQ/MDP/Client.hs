8fa02357cea3f97e6d1fcba561316b47fc313098
Mark Wotton <mwotton@gmail.com>
2012-03-16 10:43:16 +1100

    multipart send for client

--- lib/System/Network/ZMQ/MDP/Client.hs
+++ lib/System/Network/ZMQ/MDP/Client.hs
@@ -10,19 +10,26 @@ data Protocol = MDCP01
 
 data Response = Response { protocol :: Protocol,
                            service :: ByteString,
-                           response :: ByteString }
+                           response :: [ByteString] }
 
-type MDError = ByteString
+
+receiveTillEmpty :: Socket a -> IO [ByteString]
+receiveTillEmpty sock = do
+  more <- Z.moreToReceive sock
+  if more  
+    then (:) <$> receive sock [] <*> receiveTillEmpty sock
+    else return []
+
 
 send :: Socket a -> ByteString -> ByteString -> IO (Either ByteString Response)
-send sock service input =
+send sock svc input =
   do Z.send sock "MDPC01"  [SndMore]
-     Z.send sock service   [SndMore]
+     Z.send sock svc [SndMore]
      Z.send sock input  []
      maybeprot <- timeout (1000000 * 3) $ receive sock []
      case maybeprot of
        Nothing -> return $ Left "Timed out!"
        Just "MDPC01" -> do
-         res <- Response MDCP01 <$> receive sock [] <*> receive sock []
+         res <- Response MDCP01 <$> receive sock [] <*> receiveTillEmpty sock
          return $ Right res
        _ -> return $ Left "bad protocol"
