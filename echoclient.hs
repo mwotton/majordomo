0c3acf3e44201bd353f1880ad7ff8cfbcf18069c
Sean Seefried <sean.seefried@gmail.com>
2012-03-16 11:38:42 +1100

    Improved API for MDP clients.

--- echoclient.hs
+++ echoclient.hs
@@ -3,17 +3,15 @@ module Main where
 import System.ZMQ
 import Prelude hiding(getContents, putStr, putStrLn)
 import Data.ByteString.Char8 as BS
-import qualified System.Network.ZMQ.MDP.Client as MDCli
+import qualified System.Network.ZMQ.MDP.Client as C
 import Data.List as L
 
 
 main :: IO ()
 main = do
   input <- getContents
-  withContext 1 $ \c ->
-    withSocket c Req $ \sock -> do
-      connect sock "tcp://127.0.0.1:5555"
-      res <- MDCli.send sock "echo" [input]
-      putStr $ case res of
+  C.withMDPClientSocket "tcp://127.0.0.1:5555" $ \s -> do
+     res <- C.send s "echo" [input]
+     putStr $ case res of
         Left l ->  "Bad response: " `append` l
-        Right l ->  BS.concat . L.intersperse "\n" $ MDCli.response l
+        Right l ->  BS.concat . L.intersperse "\n" $ C.response l
--- lib/System/Network/ZMQ/MDP/Client.hs
+++ lib/System/Network/ZMQ/MDP/Client.hs
@@ -1,5 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
-module System.Network.ZMQ.MDP.Client where
+module System.Network.ZMQ.MDP.Client (
+  -- | Types
+  Response(..),
+  MDPClientSocket, -- opaque datatype 
+
+  -- | Functions
+  send,
+  withMDPClientSocket
+
+) where
+
 import Data.ByteString.Char8
 import qualified System.ZMQ as Z
 import System.ZMQ hiding(send)
@@ -12,6 +22,32 @@ data Response = Response { protocol :: Protocol,
                            service :: ByteString,
                            response :: [ByteString] }
 
+data MDPClientSocket = MDPClientSocket { mdpClientSocket :: Socket Req }
+
+withMDPClientSocket :: String -> (MDPClientSocket -> IO a) -> IO a
+withMDPClientSocket socketAddress io =
+ withContext 1 $ \c -> withSocket c Req $ \s -> do
+   connect s socketAddress
+   io (MDPClientSocket s)
+
+send :: MDPClientSocket -> ByteString -> [ByteString] -> IO (Either ByteString Response)
+send mdpcs service msgs =
+  do Z.send sock "MDPC01"  [SndMore]
+     Z.send sock service   [SndMore]
+     sendAll sock msgs
+     maybeprot <- timeout (1000000 * 3) $ receive sock []
+     case maybeprot of
+       Nothing -> return $ Left "Timed out!"
+       Just "MDPC01" -> do
+         res <- Response MDCP01 <$> receive sock [] <*> receiveTillEmpty sock
+         return $ Right res
+       _ -> return $ Left "bad protocol"
+  where
+    sock = mdpClientSocket mdpcs
+
+--
+-- Helper functions
+--
 
 receiveTillEmpty :: Socket a -> IO [ByteString]
 receiveTillEmpty sock = do
@@ -27,17 +63,3 @@ sendAll :: Socket a -> [ByteString] -> IO ()
 sendAll sock []     = return ()
 sendAll sock (m:[]) = Z.send sock m []
 sendAll sock (m:ms) = Z.send sock m [SndMore] >> sendAll sock ms
-
-
-send :: Socket a -> ByteString -> [ByteString] -> IO (Either ByteString Response)
-send sock svc msgs =
-  do Z.send sock "MDPC01"  [SndMore]
-     Z.send sock svc       [SndMore]
-     sendAll sock msgs
-     maybeprot <- timeout (1000000 * 3) $ receive sock []
-     case maybeprot of
-       Nothing -> return $ Left "Timed out!"
-       Just "MDPC01" -> do
-         res <- Response MDCP01 <$> receive sock [] <*> receiveTillEmpty sock
-         return $ Right res
-       _ -> return $ Left "bad protocol"
