bf1a40b41235b6495a375583a68f934fa15c3056
Sean Seefried <sean.seefried@gmail.com>
2012-03-17 13:02:44 +1100

    send -> sendAndReceive. Error messages as data type

--- lib/System/Network/ZMQ/MDP/Client.hs
+++ lib/System/Network/ZMQ/MDP/Client.hs
@@ -3,11 +3,10 @@ module System.Network.ZMQ.MDP.Client (
   -- | Types
   Response(..),
   ClientSocket, -- opaque datatype 
-
+  ClientError(..),
   -- | Functions
-  send,
+  sendAndReceive,
   withClientSocket
-
 ) where
 
 import Data.ByteString.Char8
@@ -22,29 +21,34 @@ data Response = Response { protocol :: Protocol,
                            service :: ByteString,
                            response :: [ByteString] }
 
+
 -- this can either be XReq or Req...
 data ClientSocket = ClientSocket { clientSocket :: Socket Req }
+
+
+data ClientError = ClientTimedOut
+                 | ClientBadProtocol
 
 withClientSocket :: String -> (ClientSocket -> IO a) -> IO a
 withClientSocket socketAddress io =
  withContext 1 $ \c -> withSocket c Req $ \s -> do
    connect s socketAddress
    io (ClientSocket s)
- 
 
-send :: ClientSocket -> ByteString -> [ByteString] -> IO (Either ByteString Response)
-send mdpcs svc msgs =
+
+sendAndReceive :: ClientSocket -> ByteString -> [ByteString] -> IO (Either ClientError Response)
+sendAndReceive mdpcs svc msgs =
   do -- Z.send sock "" [SndMore]
      Z.send sock "MDPC01"  [SndMore]
-     Z.send sock svc   [SndMore]
+     Z.send sock svc       [SndMore]
      sendAll sock msgs
      maybeprot <- timeout (1000000 * 3) $ receive sock []
      case maybeprot of
-       Nothing -> return $ Left "Timed out!"
+       Nothing -> return $ Left ClientTimedOut
        Just "MDPC01" -> do
          res <- Response MDCP01 <$> receive sock [] <*> receiveTillEmpty sock
          return $ Right res
-       _ -> return $ Left "bad protocol"
+       _ -> return $ Left ClientBadProtocol
   where
     sock = clientSocket mdpcs
 
--- mdp_client.hs
+++ mdp_client.hs
@@ -7,7 +7,7 @@ import qualified System.Network.ZMQ.MDP.Client as C
 import Data.List as L
 import System.Console.CmdArgs.Implicit
 
-data Client = Client {broker :: String, 
+data Client = Client {broker :: String,
                       service::String, 
                       message_parts::[String]} deriving (Show, Data, Typeable)
 
@@ -15,19 +15,20 @@ data Client = Client {broker :: String,
 client :: Client
 client = Client{ broker        = def &= argPos 0 ,
                  service       = def &= argPos 1 ,
-                 message_parts = def &= args 
+                 message_parts = def &= args
                  } &= summary "connect to a 0mq server"
 
 
 main :: IO ()
-main = do 
+main = do
   s <- cmdArgs client
   -- print s
   if (L.null $ message_parts s)
     then putStrLn "must send at least one argument"
     else C.withClientSocket (broker s) $ \sock -> do
-      res <- C.send sock (pack $ service s) (Prelude.map pack $ message_parts s)
+      res <- C.sendAndReceive sock (pack $ service s) (Prelude.map pack $ message_parts s)
       putStr $ case res of
-        Left l ->  "Bad response: " `append` l
+        Left C.ClientTimedOut    ->  "Timed out!"
+        Left C.ClientBadProtocol -> "Bad protocol!"
         Right l ->  BS.concat . L.intersperse "\n" $ C.response l
 
